140732227766212143cd67b435f1c0ef315d715b
Arash <khoshniyat.arash@hotmail.com>
2014-07-27 21:03:58 +0430

    The answer

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## well based on my instructor,good programers are lazy so,
+##I created my function from the example,simply change mean with solve and valla!!!!
+##The name changes were not needed but,its better this way.
 
-## Write a short comment describing this function
-
+## Creates a list of 4.first seting the matrix second getting it 
+## third seting the inverse and fourth getting the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+            print(y)
+      }
+      get <- function() x
+      setrev <- function(solve) m <<- solve
+      getrev <- function() m
+      list(set = set, get = get,
+           setrev = setrev,
+           getrev = getrev)
 }
 
 
-## Write a short comment describing this function
+## first checks if there is a reverse of our metrix in $getrev if its there
+##it will say that it is loading from cache and returns that reverse,
+##if there is no reverse in cache it goes on and calculats it,
+##then stores the matrix and its inverse in our list. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getrev()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setrev(m)
+      m                ## Returns a matrix that is the inverse of 'x'
+}
+        
 }
